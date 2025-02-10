bcba27d8499351d26183fae9bafc97ec3c01bf8c
SACHIN31111 <sachinkssachinks977@gmail.com>
2025-02-10 20:12:46 +0530

    oops

---
+++ Chapter 8/Class.py
@@ -0,0 +1,11 @@
+class Employee:
+    name="sachin"
+    salary=117000
+    Company="Maersk"
+
+ovaeiable=Employee()
+print(ovaeiable.name)
+object2=Employee()
+object2.name="sahana"
+print(object2.name)
+print(ovaeiable.name)
